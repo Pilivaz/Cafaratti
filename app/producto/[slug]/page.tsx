031deddc11da075d484fb672affeabe05e0d3912
Pilivaz <158507610+Pilivaz@users.noreply.github.com>
2025-07-17 20:02:20 -0300

    Update page.tsx

--- app/producto/[slug]/page.tsx
+++ app/producto/[slug]/page.tsx
@@ -12,9 +12,9 @@ const productsDatabase = {
     name: "Quilmes Clásica",
     category: "Cervezas",
     logo: "/quilmes-logo.webp", // Placeholder - se puede cambiar por logo específico de Quilmes
-    image: "/quilmes-clasica-lata-presentacion.png", // Main image (can be first of presentationImages)
-      "/quilmes-clasica-lata-presentacion.png",
+    image: "/quilmes-clasica-lata.png", // Main image (can be first of presentationImages)
       "/quilmes-clasica-lata.png",
+      "/quilmes-clasica-lata-presentacion.png",
       "/quilmes-clasica-laton.png",
       "/quilmes-clasica-botella.png",
       "/quilmes-clasica-botellaChica.png",
